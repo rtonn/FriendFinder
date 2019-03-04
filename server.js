84c1a29ec346897ec7a6ed94c1a7e561aad4f8c9
rtonn <rtonn09umn@gmail.com>
2019-03-04 12:22:58 -0600

    heroku

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//ROUTER
+require('./app/routing/apiRoutes.js')(app); 
+require('./app/routing/htmlRoutes.js')(app);
+
+// Starts the server to begin listening
+app.listen(PORT, function () {
+  console.log('App listening on PORT: ' + PORT);
+});
